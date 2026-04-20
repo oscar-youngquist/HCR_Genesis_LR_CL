915cee58eec4765c4d3fcc076f87414f90b77b99
pghezzi <100241979+pghezzi@users.noreply.github.com>
2026-04-20 11:32:46 -0400

    Small update
    Cleaned some outstanding debug code

--- legged_gym/envs/base/legged_robot.py
+++ legged_gym/envs/base/legged_robot.py
@@ -245,7 +245,6 @@ class LeggedRobot(BaseTask):
                 self.num_envs), self.action_delay].clone()
         # during training, the camera follows the first environment
         if (not self.debug) and self.headless:
-            print(f"here is the issue {self.debug} {self.headless}")
             pos = self.simulator.base_pos[0].cpu().numpy() + np.array(self.cfg.viewer.pos)
             lookat = self.simulator.base_pos[0].cpu().numpy() + np.array(self.cfg.viewer.lookat)
             self.set_viewer_camera(pos, lookat)
